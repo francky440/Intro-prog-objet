889b8347c23315f952f4738cbcc4ab5ae2579137
francky440 <franck.koye@universite-paris-saclay.fr>
2026-09-18 15:55:28 +0200

    Enhance fraction class with arithmetic and comparison methods
    Fix addition logic and add multiplication, equality, and comparison methods.

--- fraction.java
+++ fraction.java
@@ -32,8 +32,40 @@ public class fraction {
 			T.numerateur = this.numerateur + f.numerateur;
 			T.denominateur = this.denominateur;
 		} else {
-			T.numerateur = this.numerateur * f.denominateur;
-			T.denominateur = this.denominateur * f.numerateur;
+			 } else {
+			T.numerateur = (this.numerateur * f.denominateur + this.denominateur* f.numerateur);
+			T.denominateur = this.denominateur * f.denominateur;
 		}
 	}
+			
+	public fraction mul( fraction f){
+		fraction t;
+		T.numerateur = this.numerateur * f.numerateur;
+		T.denominateur = this.denominateur * f.denominateur;
+	} 
+
+	public boolean egale (fraction f){
+		return this.numerateur == f.numerateur && this.denominateur == f.denominateur;
+	} 
+
+	public int compareTo(fraction f){
+		if(this.numerateur * f.denominateur > this.denominateur * f.numerateur){
+			return 7;
+		}
+		else if (this.numerateur * f.denominateur < this.denominateur * f.numerateur){
+			return -7;
+		}
+			return 0;
+	}
+
+	public static main (){
+		fration t = new fraction ( 4, 7);
+		fraction e = new fraction( 5,2);
+		e.toString;
+		t.toString;
+		e.add(t);
+	}
+	}
+
+	
 			
